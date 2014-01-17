2ca65b37dd2ccece41cb13b3d7fde9b2ec86530c
Devin Gund <devingund@gmail.com>
2014-01-16 19:30:55 -0500

    Initial Commit

---
+++ DGCommand/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  DGCommand
+//
+//  Created by Devin Gund on 1/16/14.
+//  Copyright (c) 2014 Devin Gund. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
